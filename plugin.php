588f3ea0ff020a5e3b83fe30219751ebec07412a
Aleapp <admin@aleapp.com>
2024-10-07 20:34:57 +0000

    0.0.5

--- plugin.php
+++ plugin.php
@@ -2,13 +2,13 @@
 /**
  * Plugin Name: StartGo Contact
  * Description: StartGo Contact Form
- * Version: 0.0.4
+ * Version: 0.0.5
  * Author: Oleksiy Boyda
  * Text Domain: sgc
  */
 
-define('SGC_VERSION', '0.0.4');
+define('SGC_VERSION', '0.0.5');
 define('SGC_INDEX', plugins_url('', __FILE__));
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/src/class-load.php';
+require __DIR__ . '/src/server/class-load.php';
